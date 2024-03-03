ce2fc1f4a220ab9c18338ef4206d161afc2eab68
uuuzbf <uuuzbf@protonmail.com>
2024-03-03 14:42:57 +0100

    add patch that fixes pings and long names on scoreboard

--- src/bfhook.cpp
+++ src/bfhook.cpp
@@ -21,9 +21,26 @@ void patch_Particle_handleUpdate_crash()
     MOVE_CODE_AND_ADD_CODE(mo, 0x005389C9, 5, HOOK_ADD_ORIGINAL_AFTER);
 }
 
+void patch_scoreboard_column_widths()
+{
+    // fix ping display on scoreboard
+    // the issue is that the ping column width is only 30 (pixel?), whereas for a proper 3 digit column, atleast 35 is needed
+    // this code widens the ping column (id 6), and moves the id column (id 7) by 5 pixels, there are 3 always empty columns (8, 9, 10; all width 15)
+    // before the 3 award icon columns on the far right side, column id 8 will be squished to width 10 but its unused anyway
+    // there are two lists for the two teams, the patching needs to be done twice
+    patchBytes(0x006DFB7E, { 0x68, 0x3b, 0x01, 0, 0 }); // push 315  axis - id column (7) - pos x
+    patchBytes(0x006DFB8B, { 0x68, 0x68, 0x01, 0, 0 }); // push 360  axis - unused column (8) - pos x
+    patchBytes(0x006DFAC0, { 0x68, 0x3b, 0x01, 0, 0 }); // push 315  allied - id column (7) - pos x
+    patchBytes(0x006DFACD, { 0x68, 0x68, 0x01, 0, 0 }); // push 360  allied - unused column (8) - pos x
+    // also widen name column by squishing the unused column between name(1) and score(3)
+    patchBytes(0x006DFA7F, { 0x68, 0xAE, 0x00, 0, 0 }); // push 174  allied - unknown column (2) - pos x
+    patchBytes(0x006DFB3D, { 0x68, 0xAE, 0x00, 0, 0 }); // push 174  axis - unknown column (2) - pos x
+}
+
 void bfhook_init()
 {
     init_hooksystem(NULL);
 
     patch_Particle_handleUpdate_crash();
+    patch_scoreboard_column_widths();
 }
