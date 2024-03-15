105550f3b8f8fc3163c5d5aa80d46ae9a30ac0d5
uuuzbf <uuuzbf@protonmail.com>
2024-03-15 19:06:53 +0100

    fix alttab crash fix causing particle issues

--- src/bfhook.cpp
+++ src/bfhook.cpp
@@ -10,9 +10,8 @@ void patch_Particle_handleUpdate_crash()
     //  this index value can reach over 10k, eventually overflowing heap allocations and crashing.
     //  this bug may be responsible for glitchy particles too, when the game is maxmimized, as random stuff is 
     //  being read from the heap
-    // side effect: jet particles in SW xpack are odd
     BEGIN_ASM_CODE(mo)
-        test eax, 99
+        cmp eax, 99
         jl resume           ; to original code after jmp eax
         fstp st(0)          ; pop fpu stack, is this needed ?
         mov eax, 00538BDCh  ; jump to end of function, increments time to live and destroys if needed
